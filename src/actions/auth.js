6d0f6f76544ff4b38e9a915a6d1c03d2d80ba030
Charles Kenney <Charlesc.kenney@gmail.com>
2017-11-08 18:55:16 -0500

    actions(*): add auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,54 @@
+import firebase from 'firebase'
+
+import { firebaseAuth } from '../firebase'
+
+// action types
+export const INIT_AUTH = 'INIT_AUTH'
+export const SIGN_IN_ERROR = 'SIGN_IN_ERROR'
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS'
+export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS'
+
+export const signInError = (err) => ({
+  type: SIGN_IN_ERROR,
+  payload: err,
+})
+
+export const signInSuccess = ({ user }) => ({
+  type: SIGN_IN_SUCCESS,
+  payload: user,
+})
+
+export const authenticate = (provider) => {
+  return async dispatch => {
+    try {
+      const data = await firebaseAuth.signInWithPopup(provider)
+      dispatch(signInSuccess(data))
+    } catch (err) {
+      console.error(`Error authenticating: ${err}`)
+      dispatch(signInError(err))
+    }
+  }
+}
+
+export const signOutSuccess = () => ({
+  type: SIGN_OUT_SUCCESS,
+})
+
+export const signInWithGoogle = () => {
+  return authenticate(new firebase.auth.GoogleAuthProvider())
+}
+
+export const signInWithGithub = () => {
+  return authenticate(new firebase.auth.GithubAuthProvider())
+}
+
+export const signInWithFacebook = () => {
+  return authenticate(new firebase.auth.FacebookAuthProvider())
+}
+
+export const signOut = () => {
+  return async dispatch => {
+    await firebaseAuth.signOut()
+    dispatch(signOutSuccess())
+  }
+}
